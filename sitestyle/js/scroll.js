6451e66c2c877a15de97e89b4f182d5f7192fda5
Ormr <serhiibelman@gmail.com>
2018-09-18 23:28:28 +0300

    update 0.41

--- sitestyle/js/scroll.js
+++ sitestyle/js/scroll.js
@@ -2,7 +2,6 @@
 $(document).ready(function(){
     $("a.scroll").click(function(){
         var elementClick = $(this).attr("href");
-        // var destination = $(elementClick).offset().top;
         $("html, body").animate({scrollTop: $(elementClick).offset().top}, 900);
     });
 });
